ebc4c88ca0b0949b8d30ec390782f10783de247e
JC <junchawu@tesla.com>
2023-02-16 16:27:01 -0800

    Add VarOr

--- part_generic.go
+++ part_generic.go
@@ -17,3 +17,12 @@ func PointerTo[V any](v V) *V {
 func PtrTo[V any](v V) *V {
 	return PointerTo(v)
 }
+
+// VarOr returns the default value if the value is empty
+func VarOr[V comparable](v V, defaultValue V) V {
+	zero := new(V)
+	if v == *zero {
+		return defaultValue
+	}
+	return v
+}
--- part_generic_test.go
+++ part_generic_test.go
@@ -43,3 +43,33 @@ func TestPointerTo(t *testing.T) {
 
 	PtrTo("a")
 }
+
+func TestVarOr(t *testing.T) {
+	assertions := require.New(t)
+	assertions.Equal("t", VarOr("t", "default text"))
+	assertions.Equal("default text", VarOr("", "default text"))
+	assertions.Equal(1, VarOr(1, 99))
+	assertions.Equal(99, VarOr(0, 99))
+	assertions.Equal(int8(1), VarOr(int8(1), int8(99)))
+	assertions.Equal(int8(99), VarOr(int8(0), int8(99)))
+	assertions.Equal(int16(1), VarOr(int16(1), int16(99)))
+	assertions.Equal(int16(99), VarOr(int16(0), int16(99)))
+	assertions.Equal(int32(1), VarOr(int32(1), int32(99)))
+	assertions.Equal(int32(99), VarOr(int32(0), int32(99)))
+	assertions.Equal(int64(1), VarOr(int64(1), int64(99)))
+	assertions.Equal(int64(99), VarOr(int64(0), int64(99)))
+	assertions.Equal(uint(1), VarOr(uint(1), uint(99)))
+	assertions.Equal(uint(99), VarOr(uint(0), uint(99)))
+	assertions.Equal(uint8(1), VarOr(uint8(1), uint8(99)))
+	assertions.Equal(uint8(99), VarOr(uint8(0), uint8(99)))
+	assertions.Equal(uint16(1), VarOr(uint16(1), uint16(99)))
+	assertions.Equal(uint16(99), VarOr(uint16(0), uint16(99)))
+	assertions.Equal(uint32(1), VarOr(uint32(1), uint32(99)))
+	assertions.Equal(uint32(99), VarOr(uint32(0), uint32(99)))
+	assertions.Equal(uint64(1), VarOr(uint64(1), uint64(99)))
+	assertions.Equal(uint64(99), VarOr(uint64(0), uint64(99)))
+	assertions.Equal(float32(1), VarOr(float32(1), float32(99)))
+	assertions.Equal(float32(99), VarOr(float32(0), float32(99)))
+	assertions.Equal(float64(1), VarOr(float64(1), float64(99)))
+	assertions.Equal(float64(99), VarOr(float64(0), float64(99)))
+}
