05b6595c8dd759078d6c3e372816046baed26ebf
JC Wu <junchawu@tesla.com>
2023-05-05 14:43:32 -0700

    Add OpenOrCreate

--- go.mod
+++ go.mod
@@ -2,7 +2,10 @@ module github.com/surfinggo/mc
 
 go 1.18
 
-require github.com/stretchr/testify v1.8.0
+require (
+	github.com/pkg/errors v0.9.1
+	github.com/stretchr/testify v1.8.0
+)
 
 require (
 	github.com/davecgh/go-spew v1.1.1 // indirect
---
+++ part_file.go
@@ -0,0 +1,96 @@
+package mc
+
+import (
+	"github.com/pkg/errors"
+	"io"
+	"os"
+)
+
+type OpenOrCreateOptions struct {
+	Template     string
+	TemplatePath string
+}
+
+type OpenOrCreateOptionFunc func(o *OpenOrCreateOptions) error
+
+func WithTemplate(template string) OpenOrCreateOptionFunc {
+	return func(o *OpenOrCreateOptions) error {
+		o.Template = template
+		return nil
+	}
+}
+
+func WithTemplatePath(templatePath string) OpenOrCreateOptionFunc {
+	return func(o *OpenOrCreateOptions) error {
+		o.TemplatePath = templatePath
+		return nil
+	}
+}
+
+// EnsureFileExist ensures the file exist, creates it with a template if not.
+func EnsureFileExist(filePath string, optionFuncs ...OpenOrCreateOptionFunc) error {
+	options := &OpenOrCreateOptions{}
+	for _, optionFunc := range optionFuncs {
+		if err := optionFunc(options); err != nil {
+			return errors.Wrap(err, "apply option error")
+		}
+	}
+
+	_, err := os.Stat(filePath)
+	if err == nil {
+		return nil // already exist
+	}
+	if !os.IsNotExist(err) {
+		return errors.Wrapf(err, "os.Stat: %s error", filePath)
+	}
+
+	// not exist, create it
+	file, err := os.Create(filePath)
+	if err != nil {
+		return errors.Wrapf(err, "os.Open: %s error", filePath)
+	}
+	defer func() {
+		Must(file.Close())
+	}()
+
+	if options.Template != "" {
+		if _, err := file.WriteString(options.Template); err != nil {
+			return errors.Wrap(err, "file.WriteString error")
+		}
+		return nil
+	}
+
+	if options.TemplatePath != "" {
+		if _, err := os.Stat(options.TemplatePath); err != nil {
+			if os.IsNotExist(err) {
+				return errors.Errorf("template not exist: %s", options.TemplatePath)
+			}
+			return errors.Wrapf(err, "os.Stat: %s error", options.TemplatePath)
+		}
+
+		tmpl, err := os.Open(options.TemplatePath)
+		if err != nil {
+			return errors.Wrapf(err, "os.Open: %s error", options.TemplatePath)
+		}
+		defer func() {
+			Must(tmpl.Close())
+		}()
+
+		_, err = io.Copy(file, tmpl)
+		if err != nil {
+			return errors.Wrap(err, "io.Copy error")
+		}
+	}
+
+	// no template, just create an empty file
+	return nil
+}
+
+// OpenOrCreate opens the file, creates it with a template if not.
+func OpenOrCreate(filePath string, optionFuncs ...OpenOrCreateOptionFunc) (*os.File, error) {
+	if err := EnsureFileExist(filePath, optionFuncs...); err != nil {
+		return nil, err
+	}
+	return os.Open(filePath)
+
+}
